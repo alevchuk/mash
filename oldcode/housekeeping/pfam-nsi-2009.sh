e3eb57ac2e5f5233d36f1113561fffcb5d2fc209
Aleksandr Levchuk <alevchuk@gmail.com>
2010-12-30 15:22:51 -0800

    Removing oldcode

--- oldcode/housekeeping/pfam-nsi-2009.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home_girkelab/alevchuk/projects/5-clustalw-to-html/housekeeping
-time hmmpfam --cpu 8 --acc -E 1e-4 /srv/projects/db/PFAM/Pfam_ls ../../4-update-families/newfam_2005_NSI.fasta | /home_girkelab/alevchuk/projects/1-arabiclust/scripts/filter-hmmpfam-output > newfam_2009_NSI.pfams
